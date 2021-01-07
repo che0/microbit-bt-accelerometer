12b14e78b991f7d39da16cc90add679023b2f1b0
Petr Novak <petr.novak@cosmoboy.cz>
2021-01-08 00:06:03 +0100

    flake8

--- linux-client/btsensor.py
+++ linux-client/btsensor.py
@@ -3,9 +3,10 @@ import bluepy
 import struct
 import os
 
-
-MICROBIT_ACCELEROMETER_PERIOD_UUID = 'e95dfb24-251d-470a-a062-fa1922dfa9a8'
-MICROBIT_ACCELEROMETER_DATA_UUID = 'e95dca4b-251d-470a-a062-fa1922dfa9a8'
+CLIENT_CHARACTERISTICS_CONF_UUID = '00002902-0000-1000-8000-00805f9b34fb'
+MB_ACCELEROMETER_SERVICE_UUID = 'e95d0753-251d-470a-a062-fa1922dfa9a8'
+MB_ACCELEROMETER_DATA_UUID = 'e95dca4b-251d-470a-a062-fa1922dfa9a8'
+MB_ACCELEROMETER_PERIOD_UUID = 'e95dfb24-251d-470a-a062-fa1922dfa9a8'
 
 
 class AccelerometerDelegate(bluepy.btle.DefaultDelegate):
@@ -25,17 +26,18 @@ def main():
     mac = os.getenv('MICROBIT_MAC')
     if not mac:
         print('We need micro:bit Bluetooth MAC address in MICROBIT_MAC\n'
-            'You can try `bluetoothctl scan on` or similar tools to find it')
+              'You can try `bluetoothctl scan on` or similar tools to find it')
         return
 
     microbit = bluepy.btle.Peripheral(mac, 'random')
 
-    accelerometer = microbit.getServiceByUUID('e95d0753-251d-470a-a062-fa1922dfa9a8')
-    acc_data_descriptor = accelerometer.getDescriptors(MICROBIT_ACCELEROMETER_DATA_UUID)[0]
-    microbit.setDelegate(AccelerometerDelegate(acc_data_descriptor.handle))
+    acc_service = microbit.getServiceByUUID(MB_ACCELEROMETER_SERVICE_UUID)
+    acc_data = acc_service.getDescriptors(MB_ACCELEROMETER_DATA_UUID)[0]
+    microbit.setDelegate(AccelerometerDelegate(acc_data.handle))
 
-    client_characteristics = accelerometer.getDescriptors('00002902-0000-1000-8000-00805f9b34fb')[0]
-    client_characteristics.write(struct.pack('<H', 1), withResponse=True)
+    # enable notifications
+    ccc = acc_service.getDescriptors(CLIENT_CHARACTERISTICS_CONF_UUID)[0]
+    ccc.write(struct.pack('<H', 1), withResponse=True)
 
     while True:
         microbit.waitForNotifications(1.0)
